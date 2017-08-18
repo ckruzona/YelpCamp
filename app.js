d43b7de8178a077f6e13720cec94124c6641b11f
Chris Kruzona <chriskruzona@gmail.com>
2017-08-18 03:29:21 +0000

    Added Flash messages for success and errors

--- app.js
+++ app.js
@@ -7,20 +7,23 @@ var methodOverride = require("method-override"),
     express        = require("express"),
     Comment        = require("./models/comment"),
     seedDB         = require("./seeds"),
+    flash          = require("connect-flash"),
     User           = require("./models/user"),
     app            = express();
+    
 
 // REQUIRING ROUTES   
 var campgroundRoutes = require("./routes/campgrounds"),
     commentRoutes    = require("./routes/comments"),
-    indexRoutes       = require("./routes/index")
+    indexRoutes       = require("./routes/index");
     
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
+app.use(flash());
 // seedDB(); //seed the database
 
 // PASSPORT CONFIGURATION
@@ -37,6 +40,8 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next){
     res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
     next();
 });
 
